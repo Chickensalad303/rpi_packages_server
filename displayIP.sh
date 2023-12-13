f2c57ccd00f3dbb0ad9f5d74fcc91b17db6c7f16
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-12-13 18:19:59 +0100

    added linux popup view ip

---
+++ displayIP.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+zenity --display=:0.0 --info --title="LAN IP Address" --text="Your LAN IP Address is:\n $(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*' | tr '\n' '\n')"
+#displays the ipv4 adress of the raspberry pi, in a popup window for client to view
