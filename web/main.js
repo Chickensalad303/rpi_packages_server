107fd713d33c633211ce2b6d9e03a22455605709
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-12-13 15:35:32 +0100

    auto gets newest data every 2 seconds now

--- web/main.js
+++ web/main.js
@@ -1,28 +1,75 @@
 
-const apiUrl = "192.168.178.20:3000/api"
-const options = {
-  "headers": {
-    "Access-Control-Allow-Origin" : true
-  }
+// const options = {
+  //   "headers": {
+    //     "Access-Control-Allow-Origin" : true
+    //   }
+    // }
+
+
+
+const apiUrl = `${window.location.origin}/api`
+console.log(apiUrl)
+
+
+
+function removeOld() {
+  var elements = document.getElementsByClassName("dynamicList")
+  Array.from(elements).forEach((element) => {
+    element.remove()
+  })
+
 }
 
 
-document.getElementById("butt").addEventListener("click", () => {
-  
-  
-  
-    var xhttp = new XMLHttpRequest();
+function createList(array) {
+  removeOld()
+  for (let i = 0; i < array.length; i++) {
+    var current = array[i]
     
+    const para = document.createElement("p")
+    const node = document.createTextNode(current)
+    para.className = "dynamicList"
+    para.appendChild(node)
     
-    xhttp.onreadystatechange = (err) => {
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        document.getElementById("demo").innerHTML = xhttp.responseText;
-        console.log("here")
+    const element = document.getElementById("list")
+    element.appendChild(para)
+  }
+}
+
+function getData() {
+  var xhttp = new XMLHttpRequest();
+  //get request to api, returns array with the names with iterable objects inside
+  xhttp.open("GET", apiUrl, true)
+  xhttp.send()
+  
+  
+  xhttp.onreadystatechange = (err) => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var response = JSON.parse(xhttp.responseText)
+
+  
+      var nameList = []
+      for (let i = 0; i < response.length; i++) {
+        var currentName = response[i].name
+        nameList.push(currentName)
       }
-    };
+      createList(nameList)
   
-    //get request to api, returns array with the names with iterable objects inside
-    xhttp.open("GET", "http://192.168.178.20:3000/api", true)
-    xhttp.send()
-  })
+    } else {
+      console.log(err)
+    }
+  };
+  
+
+}
+
+// document.getElementById("butt").addEventListener("click", () => {
+  
+//   getData()
+// })
+
+setInterval(() => {
+  getData()
+  console.log("hiii")
+}, 2000)
   
