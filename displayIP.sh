3a6568568192324a2333ff5b4d771c3e3d80875f
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-12-13 18:22:52 +0100

    should be finished now

--- displayIP.sh
+++ displayIP.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-zenity --display=:0.0 --info --title="LAN IP Address" --text="Your LAN IP Address is:\n $(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*' | tr '\n' '\n')"
+zenity --display=:0.0 --info --title="Server IP Address" --text="the servers IP Address is:\n $(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*' | tr '\n' '\n'):3000"
 #displays the ipv4 adress of the raspberry pi, in a popup window for client to view
 #just run chmod +x displayIP.sh to make it executable, then do ./displayIP.sh
